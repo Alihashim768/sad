697111386b95f9e9875ab4bc8cff9106e9a719e9
Alihashim768 <85179965+Alihashim768@users.noreply.github.com>
2025-04-23 13:52:28 +0500

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,12 +1,16 @@
 import os, platform, sys
+import traceback
 
 os.system('git pull')
 sys.path.append('/sdcard')
 
 bit = platform.architecture()[0]
+print(f"System Architecture: {bit}")
+
 if bit == '64bit':
     try:
         import tata
         print("Imported tata module successfully.")
     except Exception as e:
-        print("Failed to import tata:", e)
+        print("Failed to import tata module.")
+        traceback.print_exc()
